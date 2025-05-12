eae058fbb30896b228998aab515eb9da2805fb16
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2025-05-12 22:19:19 +0200

    Add pinning to tinymist

--- dot-config/nvim/lsp/tinymist.lua
+++ dot-config/nvim/lsp/tinymist.lua
@@ -62,5 +62,36 @@ return {
         { nargs = 0, desc = cmd_desc }
       )
     end
+    vim.keymap.set(
+      "n",
+      "<leader>tp",
+      function()
+        client:exec_cmd({
+
+          title = "pin",
+
+          command = "tinymist.pinMain",
+
+          arguments = { vim.api.nvim_buf_get_name(0) },
+        }, { bufnr = bufnr })
+      end,
+      { desc = "[T]inymist [P]in", noremap = true }
+    )
+
+    vim.keymap.set(
+      "n",
+      "<leader>tu",
+      function()
+        client:exec_cmd({
+
+          title = "unpin",
+
+          command = "tinymist.pinMain",
+
+          arguments = { vim.v.null },
+        }, { bufnr = bufnr })
+      end,
+      { desc = "[T]inymist [U]npin", noremap = true }
+    )
   end,
 }

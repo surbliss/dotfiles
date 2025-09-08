98cd60ff154cbb7ce2e5ee7b972c55100611a092
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2025-09-08 23:10:33 +0200

    Fix wezterm clipboard

--- dot-config/wezterm/wezterm.lua
+++ dot-config/wezterm/wezterm.lua
@@ -7,6 +7,7 @@ config.term = "xterm-256color"
 config.use_fancy_tab_bar = false
 config.color_scheme = "Catppuccin Mocha"
 -- config.color_scheme = "Jellybeans"
+config.color_scheme = "Kanagawa Dragon (Gogh)"
 config.harfbuzz_features = { "ss01" } -- Script italics
 config.font = wezterm.font_with_fallback {
   "0xProto Nerd Font",
@@ -23,7 +24,7 @@ config.font = wezterm.font_with_fallback {
 }
 -- NOTE: Precisely 80 characters:
 -- 45678901234567890123456789012345678901234567890123456789012345678901234567890
-config.font_size = 10
+config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
 config.adjust_window_size_when_changing_font_size = false
 
@@ -94,8 +95,13 @@ config.keys = {
   --   },
   {
     key = "v",
-    mods = "SHIFT|CTRL",
+    mods = "CTRL",
     action = act.PasteFrom "Clipboard",
+  },
+  {
+    key = "V",
+    mods = "CTRL",
+    action = act.PasteFrom "PrimarySelection",
   },
   {
     key = "Backspace", mods = "CTRL", action = act.SendKey { key = "w", mods = "CTRL" }
